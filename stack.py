e866136a1e0694d5d3d8efda5e7cd7ab11b5a519
TavernInHeaven <nty2013@yahoo.com>
2018-08-18 12:16:05 +1000

    List functions cannot be use in stack, why? Why we have to use self.item everywhere?

---
+++ stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.item = []
+
+    def pop(self):
+        return self.item.pop()
+
+    def peek(self):
+        return self.item[len(self.item) - 1]
+
+    def push(self,item):
+        self.item.append(item)
+
+    def is_empty(self):
+        return self.item == []
+
+    def size(self):
+        return len(self.item)
+
+tryList = []
+print(tryList.__eq__([]))
+
+tryStack = Stack()
+print(tryStack.is_empty())
+tryStack.push(4)
+tryStack.push('dog')
+print(tryStack.peek())
+tryStack.push(True)
+print(tryStack.size())
+print(tryStack.is_empty())
+tryStack.push(8.4)
+print(tryStack.pop())
+print(tryStack.pop())
+print(tryStack.size())
+print(tryStack.pop())
+print(tryStack.__eq__([4]))
