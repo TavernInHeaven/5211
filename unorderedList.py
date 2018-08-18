c96a12a4ed3894c54856607c07c90019f62bd2b8
TavernInHeaven <nty2013@yahoo.com>
2018-08-18 22:11:55 +1000

    666

---
+++ unorderedList.py
@@ -0,0 +1,22 @@
+from node import Node
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empyt(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.get_next()
+
+        return count
